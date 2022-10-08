f485ab689ba822885132c10637e94cb3229ce017
Andra <94204279+asteele1493@users.noreply.github.com>
2022-10-08 15:53:11 -0700

    Added error message

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import axios from 'axios';
 import CityDisplay from './CityDisplay';
+import { Alert } from 'react-bootstrap';
+// import Alert from 'react-bootstrap/Alert';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,7 +12,9 @@ class App extends React.Component {
       searchQuery: '',
       location: {},
       lat: '',
-      lon: ''
+      lon: '',
+      err: 'Error: Unable to Geocode!',
+      apiError: false
     }
   }
 
@@ -19,18 +23,25 @@ class App extends React.Component {
   }
 
   getLocation = async () => {
-    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
-    const otherUrl =  `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
-    // console.log('URL:', url);
-    //call axios
-    const response = await axios.get(url, otherUrl);
-    console.log('response object: ', response);
-    console.log('response.data[0]: ', response.data[0]);
-    this.setState({ location: response.data[0] });
-    this.setState({ lat: response.data[0].lat });
-    this.setState({ lon: response.data[0].lon });
-    console.log('response object : ', response.data[0].lat);
-    console.log('response object : ', response.data[0].lon);
+    try {
+      const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
+      const otherUrl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
+      // console.log('URL:', url);
+      //call axios
+      const response = await axios.get(url, otherUrl);
+      this.setState({ apiError: false });
+      console.log('response object: ', response);
+      console.log('response.data[0]: ', response.data[0]);
+      this.setState({ location: response.data[0] });
+      this.setState({ lat: response.data[0].lat });
+      this.setState({ lon: response.data[0].lon });
+      console.log('response object : ', response.data[0].lat);
+      console.log('response object : ', response.data[0].lon);
+    } catch (err) {
+      this.setState({ apiError: true });
+      this.setState({location:{}})
+      console.error(this.state.err);
+    }
   }
 
 
@@ -44,17 +55,22 @@ class App extends React.Component {
           placeholder="search for a city"
         />
         <button onClick={this.getLocation}>Explore!</button>
+        {this.state.apiError &&
+          <Alert variant="danger">
+            <Alert.Heading>Error: Unable to geocode!</Alert.Heading>
+          </Alert>
+        }
+        {this.state.location.display_name &&
 
-        {this.state.location.display_name && 
-          
-        <CityDisplay
-          location = {this.state.location}
-          lat = {this.state.lat}
-          lon = {this.state.lon}
-        />
-      }
+          <CityDisplay
+            location={this.state.location}
+            lat={this.state.lat}
+            lon={this.state.lon}
+          />
+
+        }
       </div>
-      
+
 
     );
   }
